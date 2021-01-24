3afaf52728304aa9d45719eb33dbb550fb97662a
tfilla <tfilla.primary@gmail.com>
2021-01-24 16:29:12 -0600

    adding styles, updating content

--- public/js/empty.js
+++ public/js/empty.js
@@ -2,7 +2,8 @@ var empty = new Vue({
     el: '#app',
     data: function() {
         return {
-            count: 1
+            count: 1,
+            blogs: ['blog post one', 'plog lost two']
         }
     },
     methods: {
